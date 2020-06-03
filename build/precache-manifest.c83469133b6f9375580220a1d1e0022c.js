b3bd6820eaad634bd68ebfac5df3c7652175931b
grudge17 <shresthpal17@gmail.com>
2020-06-04 01:12:34 +0530

    frontend

---
+++ build/precache-manifest.c83469133b6f9375580220a1d1e0022c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4e8e52cdb869998a9558c81ab08217",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "08b8f92016800a4f0120",
+    "url": "/public/static/css/main.33dba587.chunk.css"
+  },
+  {
+    "revision": "1b105ffad28ad961aaa9",
+    "url": "/public/static/js/2.99eb9c3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/public/static/js/2.99eb9c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545037a6d72919186508",
+    "url": "/public/static/js/3.5fb1c167.chunk.js"
+  },
+  {
+    "revision": "a8636824062a07280db2",
+    "url": "/public/static/js/4.004b67ec.chunk.js"
+  },
+  {
+    "revision": "08b8f92016800a4f0120",
+    "url": "/public/static/js/main.bf1dccd2.chunk.js"
+  },
+  {
+    "revision": "1dc5fbd1f32f465a3a20",
+    "url": "/public/static/js/runtime-main.28f35828.js"
+  }
+]);
